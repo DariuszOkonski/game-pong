50a448e1e5d86c42efc05ee66382d08d45735d2d
Dariusz Okonski <darek200180@gmail.com>
2022-06-16 15:43:44 +0100

    make ball bouncing on sides

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 let canvas;
 let canvasContext;
+let ballX = 50;
+let ballSpeedX = 5;
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
-  canvasContext.fillStyle = "red";
-  canvasContext.fillRect(220, 100, 50, 20);
-  canvasContext.fillStyle = "green";
-  canvasContext.fillRect(350, 200, 50, 50);
+  const framesPerSecond = 30;
+  setInterval(function () {
+    moveEverything();
+    drawEverything();
+  }, 1000 / framesPerSecond);
 };
+
+function moveEverything() {
+  ballX = ballX + ballSpeedX;
+
+  if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX > canvas.clientWidth) {
+    ballSpeedX = -ballSpeedX;
+  }
+}
+
+function drawEverything() {
+  colorRect(0, 0, canvas.clientWidth, canvas.clientHeight, "black");
+
+  colorRect(0, 100, 10, 100, "white");
+
+  colorRect(ballX, 110, 10, 10, "white");
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
+}

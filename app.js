a3455e438dd610a0d045b5ff8f680590e559e36a
Dariusz Okonski <darek200180@gmail.com>
2022-06-16 18:59:29 +0100

    make paddles move

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ let ballSpeedX = 10;
 let ballSpeedY = 4;
 
 let paddle1Y = 250;
+let paddle2Y = 250;
+const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
+
+var player1Score = 0;
+var player2Score = 0;
 
 function calculateMousePos(evt) {
   // it's taking coordinates only from canvas regardless of scroll
@@ -37,15 +42,42 @@ window.onload = function () {
   });
 };
 
+function ballReset() {
+  ballSpeedX = -ballSpeedX;
+  ballX = canvas.clientWidth / 2;
+  ballY = canvas.clientHeight / 2;
+}
+
+function computerMovement() {
+  var paddle2YCenter = paddle2Y + PADDLE_HEIGHT / 2;
+  if (paddle2YCenter < ballY - 35) {
+    paddle2Y += 6;
+  } else if (paddle2YCenter > ballY + 35) {
+    paddle2Y += -6;
+  }
+}
+
 function moveEverything() {
-  ballX = ballX + ballSpeedX;
-  ballY = ballY + ballSpeedY;
+  computerMovement();
+
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
 
   if (ballX < 0) {
-    ballSpeedX = -ballSpeedX;
+    if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+      player2Score++;
+    }
   }
   if (ballX > canvas.clientWidth) {
-    ballSpeedX = -ballSpeedX;
+    if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+      player1Score++;
+    }
   }
 
   if (ballY < 0) {
@@ -61,11 +93,23 @@ function drawEverything() {
   //play court
   colorRect(0, 0, canvas.clientWidth, canvas.clientHeight, "black");
 
-  //left paddle
-  colorRect(0, paddle1Y, 10, PADDLE_HEIGHT, "white");
+  //left player paddle
+  colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, "white");
+
+  // right computer paddle
+  colorRect(
+    canvas.clientWidth - PADDLE_THICKNESS,
+    paddle2Y,
+    PADDLE_THICKNESS,
+    PADDLE_HEIGHT,
+    "white"
+  );
 
   //draws the ball
-  colorCircle(ballX, ballY, 10, "red");
+  colorCircle(ballX, ballY, 10, "white");
+
+  canvasContext.fillText(player1Score, 100, 100);
+  canvasContext.fillText(player2Score, canvas.clientWidth - 100, 100);
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
